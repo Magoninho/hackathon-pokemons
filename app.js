d50a243ef59a96807955194bf177251c0ac4b80b
Magoninho <magoninhogamer@gmail.com>
2021-12-21 17:17:50 -0300

    not working yet

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let limit = 9;
+
 function printJson(json) {
 	document.body.innerHTML += JSON.stringify(json);
 }
@@ -6,32 +8,48 @@ function printmsg(msg) {
 	document.body.innerHTML += msg + "<br>";
 }
 
-function renderPokemon(sprites, name, types) {
-	let div = document.createElement("div");
-	div.classList.add('pokemon_info');
+function renderPokemons(pokemons, mainDiv) {
 
-	let pokemonNameElem = document.createElement("p");
-	let image = document.createElement("img");
+	mainDiv.innerHTML = "";
+	for (let p = 0; p < pokemons.length; p++) {
+		const pokemon = pokemons[p]; // current pokemon
+		let div = document.createElement("div");
+		div.classList.add('pokemon_info');
 
-	image.src = `${sprites.other.dream_world.front_default}`;
-
-	div.appendChild(image);
-
-	pokemonNameElem.innerHTML += `Nome: ${name}<br>`;
-
-	// adding types
-	firstTypeName = types[0].type.name
-	div.classList.add(`pokemon_${firstTypeName}`);
-	types.forEach(type => {
-		pokemonNameElem.innerHTML += `Tipo: ${type.type.name}<br>`;
-	})
+		let pokemonNameElem = document.createElement("p");
+		let image = document.createElement("img");
 
 
-	div.appendChild(pokemonNameElem);
 
-	// appending div
+		image.src = `${pokemon.sprites.other.dream_world.front_default}`;
 
-	document.getElementById("pokemons").appendChild(div);
+		div.appendChild(image);
+
+		pokemonNameElem.innerHTML += `Nome: ${pokemon.name}<br>`;
+
+		// adding types
+		firstTypeName = pokemon.types[0].type.name
+		div.style.backgroundColor = `var(--${firstTypeName})`;
+		pokemon.types.forEach(type => {
+			const typeName = type.type.name;
+			pokemonNameElem.innerHTML += `Tipo: ${typeName}<br>`;
+		})
+
+
+		div.appendChild(pokemonNameElem);
+
+		// appending div
+
+		document.getElementById("pokemons").appendChild(div);
+	}
+}
+
+async function getResults() {
+	const URL = "https://pokeapi.co/api/v2/pokemon";
+	const response = await fetch(URL);
+	const data = await response.json();
+	return data;
+	
 }
 
 async function getPokemons(from, to) {
@@ -48,21 +66,72 @@ async function getPokemons(from, to) {
 	return pokemons;
 }
 
-async function generatePokemons() {
-	const pokemons = await getPokemons(0, 500);
+async function getPokemonData(offset, limit) {
+	const pokemons = await getPokemons(offset, limit);
+	let data = [];
 
 	for (let p = 0; p < pokemons.length; p++) {
 		const pokemonResp = await fetch(pokemons[p].url);
 		const pokemonData = await pokemonResp.json();
 
-		renderPokemon(pokemonData.sprites, pokemonData.name, pokemonData.types);
+		data.push(pokemonData);
+		//renderPokemon(pokemonData.sprites, pokemonData.name, pokemonData.types);
 	}
+	return data;
 }
 
+function searchPokemon(regex, pokemonData) {
+	let foundPokemons = [];
+	console.log(pokemonData)
+	for (let p = 0; p < pokemonData.length; p++) {
+		const pokemon = pokemonData[p]; // pokemon element
+		// searching the pokemon
+		if (pokemon.name.match(regex)) {
+			// pushing the found pokemons
+			foundPokemons.push(pokemon);
+		}
+	}
 
-let input = document.getElementById("search_pokemon");
-let btn = document.getElementById("search_button");
 
-generatePokemons();
+	return foundPokemons;
+}
 
+(async () => {
+	const myDiv = document.getElementById("pokemons");
+	const pokemonData = await getPokemonData(0, 9);
+	await renderPokemons(pokemonData, myDiv);
+
+	let input = document.getElementById("search_pokemon");
+
+	await input.addEventListener("input", (async () => {
+		let offset = 0;
+
+		// searching pokemons with the input value
+		// let pokemonSearch = searchPokemon(new RegExp(`^${input.value.trim().toLowerCase()}`), pokemonData);
+
+		let found = false;
+		while (!found) {
+			let pokemonSearch = searchPokemon(new RegExp(`^${input.value.trim().toLowerCase()}`), (await getPokemonData(offset, 32)));
+			offset += 32;
+			renderPokemons(pokemonSearch, myDiv);
+		}
+		console.log("achou")
+
+	}));
+
+	let btn_more = document.getElementById("btn-more");
+	
+	
+	await btn_more.addEventListener("click", (async () => {
+		const newPokemonData = await getPokemonData(limit, 9);
+		for (let i = 0; i < newPokemonData.length; i++) {
+			pokemonData.push(newPokemonData[i]);
+		}
+		limit += 9;
+		// const newPokemonSearch = searchPokemon(new RegExp(`^${input.value.trim().toLowerCase()}`), pokemonData);
+
+
+		renderPokemons(newPokemonSearch, myDiv);
+	}));
+})();
 
